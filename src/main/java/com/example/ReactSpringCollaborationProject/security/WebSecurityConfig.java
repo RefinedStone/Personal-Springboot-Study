a34a129944861780bce03fbb94138ad81b9fb765
RefinedStone <chem.en.9273@gmail.com>
2022-10-26 02:39:08 +0900

    aws ec2 server + s3 + mysql 서버 세팅 완료

--- src/main/java/com/example/ReactSpringCollaborationProject/security/WebSecurityConfig.java
+++ src/main/java/com/example/ReactSpringCollaborationProject/security/WebSecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -31,15 +36,16 @@ public class WebSecurityConfig {
         return (web) -> web.ignoring().antMatchers("/h2-console/**");
     }
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.cors();
+        //cors 설정
+        http.cors().configurationSource(configurationSource());
         http.csrf().disable();
                 //ignoringAntMatchers("/user/**");
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
         http.authorizeRequests().antMatchers("/api/**").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
@@ -50,4 +56,19 @@ public class WebSecurityConfig {
         return http.build();
 
     }
+
+    //cors 설정?
+    @Bean
+    public CorsConfigurationSource configurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("POST","GET","DELETE","PUT", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
